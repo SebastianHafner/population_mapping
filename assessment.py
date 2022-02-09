dfab5f7264460191426924283692c58d8ec831c9
SebastianHafner <hafnersailing@gmail.com>
2022-02-09 18:41:10 +0100

    fixed scale on correlation plot

--- assessment.py
+++ assessment.py
@@ -212,7 +212,20 @@ def correlation_census(cfg: experiment_manager.CfgNode, city: str, run_type: str
     textstr = r'$R^2 = {r_value:.2f}$'.format(r_value=r_value)
     ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=FONTSIZE,
             verticalalignment='top')
-    pop_max = 30_000
+    gt_max, pred_max = np.max(gts), np.max(preds)
+    pop_max = gt_max if gt_max > pred_max else pred_max
+    if pop_max < 1_000:
+        pop_max = 1_000
+    elif pop_max < 5_000:
+        pop_max = 5_000
+    elif pop_max < 10_000:
+        pop_max = 10_000
+    elif pop_max < 50_000:
+        pop_max = 50_000
+    elif pop_max < 100_000:
+        pop_max = 100_000
+    else:
+        pop_max = 1_000_000
 
     ax.plot([0, pop_max], [0, pop_max], c='k', zorder=-1, label='1:1 line')
 
@@ -265,31 +278,6 @@ def produce_population_grid(cfg: experiment_manager.CfgNode, city: str):
             arr[i, j, 0] = pred_pop
     out_file = Path(cfg.PATHS.OUTPUT) / 'population_grids' / f'pop_{city}_{cfg.NAME}.tif'
     geofiles.write_tif(out_file, arr, transform, crs)
-
-
-def produce_error_grid(config_name: str, city: str, dataset_path: str, output_path: str):
-    cfg = experiment_manager.load_cfg(config_name)
-    ds = datasets.CellPopulationDataset(cfg, run_type, no_augmentations=True)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    net, *_ = networks.load_checkpoint(cfg.INFERENCE_CHECKPOINT, cfg, device)
-    net.eval()
-
-    pop_file = Path(dirs.DATASET) / 'population_data' / f'pop_{city}.tif'
-    pop_arr, transform, crs = geofiles.read_tif(pop_file)
-
-    m, n, _ = pop_arr.shape
-    error = np.full((m, n, 3), fill_value=np.NaN, dtype=np.single)
-    for item in tqdm(ds):
-        x = item['x'].to(device)
-        i, j = item['i'], item['j']
-        pred_pop = net(x.unsqueeze(0)).flatten().cpu().item()
-        pop = item['y'].item()
-        error[i, j, 0] = pop - pred_pop
-        error[i, j, 1] = pop
-        error[i, j, 2] = pred_pop
-
-    out_file = Path(dirs.OUTPUT) / f'error_{config_name}_{run_type}_{city}.tif'
-    geofiles.write_tif(out_file, error, transform, crs)
 
 
 if __name__ == '__main__':
