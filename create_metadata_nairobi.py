e1a0d491c552a36c15ade6acfe70f17972ae8338
SebastianHafner <hafnersailing@gmail.com>
2022-02-16 14:49:36 +0100

    updated dataset and configs

--- create_metadata_nairobi.py
+++ create_metadata_nairobi.py
@@ -2,6 +2,7 @@ import numpy as np
 from pathlib import Path
 from utils import geofiles, parsers
 import geopandas as gpd
+import numpy as np
 
 
 def assemble_metadata(features: list, raw_data_path: str, dataset_path: str):
@@ -13,17 +14,17 @@ def assemble_metadata(features: list, raw_data_path: str, dataset_path: str):
         'samples': [],  # list with all the samples
     }
 
-    census_file = Path(raw_data_path) / city / f'census_areas_{city}.shp'
+    census_file = Path(raw_data_path) / city / f'census.shp'
     gdf = gpd.read_file(census_file)
 
     # rasterized version of census file with 'cat' as pixel value (0 is no census)
-    units_file = Path(raw_data_path) / city / f'units_{city}.tif'
+    units_file = Path(raw_data_path) / city / f'raster_census_units.tif'
     units, _, _ = geofiles.read_tif(units_file)
 
     for index, unit in np.ndenumerate(units):
         i, j, _ = index
         patch_id = f'{i:03d}-{j:03d}'
-        if unit == 0:
+        if unit == 255:
             for feature in features:
                 patch_file = Path(dataset_path) / 'features' / city / feature / f'{feature}_{city}_{patch_id}.tif'
                 if patch_file.exists():
@@ -44,7 +45,7 @@ def assemble_metadata(features: list, raw_data_path: str, dataset_path: str):
 
     for index, row in gdf.iterrows():
         unit_nr = row['cat']
-        unit_pop = row['value']
+        unit_pop = row['Pop_2019']
         metadata['census'][unit_nr] = unit_pop
         metadata['split'][unit_nr] = 'test'
 
